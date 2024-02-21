922bcac62f7be042f144647c001d0f4440f925d3
Kevin Fenn <boyce.fenn@gmail.com>
2024-02-21 14:49:54 -0600

    revised ISA and added switch statement for INS

---
+++ cpu.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#define     R_W     0b0000
+#define     R_X     0b0001
+#define     R_Y     0b0010
+#define     R_Z     0b0011
+#define     R_PC    0b0100
+#define     R_PCU   0b0101
+#define     R_AR    0b0110
+#define     R_ARU   0b0111
+#define     R_SP    0b1000
+#define     R_SR    0b1001
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <cstdint>
 #include <iostream>
+#include "cpu.hpp"
+
+int cycles = 0;
 
 struct registers{
-    uint8_t gReg[4];
-    uint8_t SR;
-    uint8_t SP;
-    uint16_t PC;
+    uint8_t reg[0b1010]; // for clarity use cpu.hpp macros when accessing these
 };
 struct memory{
     uint8_t stack[265]; //stack, nuff said.
@@ -22,20 +22,21 @@ int main(){
     registers cpuReg;
 
     initializeCPU(cpuReg, cpuMem);
-    execute(cpuReg, cpuMem);
-    std::cout << cpuReg.W << std::endl;
+    for(int i = 0; i < 16; i++){
+            execute(cpuReg, cpuMem);
+    }
+    
+    std::cout << "FINISHED!" << std::endl;
     return 0;
 }
 
 void initializeCPU(registers& cpuReg, memory& cpuMem) {
+    
     // Initialize CPU registers
-    for (int i = 0; i < 4; ++i) {
-        cpuReg.gReg[i] = 0;
+    for (int i = 0; i < 10; i++) {
+        cpuReg.reg[i] = 0;
     }
-    cpuReg.PC = 0;
-    cpuReg.SP = 0;
-    cpuReg.SR = 0;
-
+    
     // Initialize CPU memory
     for (int i = 0; i < 265; ++i) {
         cpuMem.stack[i] = 0;
@@ -47,37 +48,104 @@ void initializeCPU(registers& cpuReg, memory& cpuMem) {
         cpuMem.data[i] = 0;
     }
 
-    cpuMem.ins[0] = 0b1000101100000111;
+    cpuMem.ins[0]   =   0b0000101100000111;
+    cpuMem.ins[1]   =   0b0001101100000111;
+    cpuMem.ins[2]   =   0b0010101100000111;
+    cpuMem.ins[3]   =   0b0011101100000111;
+    cpuMem.ins[4]   =   0b0100101100000111;
+    cpuMem.ins[5]   =   0b0101101100000111;
+    cpuMem.ins[6]   =   0b0110101100000111;
+    cpuMem.ins[7]   =   0b0111101100000111;
+    cpuMem.ins[8]   =   0b1000101100000111;
+    cpuMem.ins[9]   =   0b1001101100000111;
+    cpuMem.ins[10]  =   0b1010101100000111;
+    cpuMem.ins[11]  =   0b1011101100000111;
+    cpuMem.ins[12]  =   0b1100101100000111;
+    cpuMem.ins[13]  =   0b1101101100000111;
+    cpuMem.ins[14]  =   0b1110101100000111;
+    cpuMem.ins[15]  =   0b1111101100000111;
+    
+
 
 }
 
 void execute(registers& cpuReg, memory& cpuMem){
     uint16_t ins = fetchIns(cpuReg, cpuMem);
     uint8_t opcode = ins >> 12;
-    uint8_t mode = ((ins>>11) & 0b1);
-    uint8_t operand1 = (ins >> 8)& 0x03;
 
     switch (opcode)
     {
-    case 0b1000:
-        std::cout <<"added!"<<std::endl;
-        if(mode == 0b1){
-            uint8_t immediate = ins & 0xFF;
-            cpuReg.gReg[operand1] =
-            std::cout <<(int)operand1<<std::endl;
-        }
+    // case 0b1000:
+    //     std::cout <<"added!"<<std::endl;
+    //     if(mode == 0b1){
+    //         uint8_t immediate = ins & 0xFF;
+    //         cpuReg.gReg[operand1] =
+    //         std::cout <<(int)operand1<<std::endl;
+    //     }
+    //     break;
+    case 0b0000:    //NOP
+        std::cout << "NOP" << std::endl;
         break;
-    
+    case 0b0001:    //ADD
+        std::cout << "ADD" << std::endl;
+        break;
+    case 0b0010:    //ADDI
+        std::cout << "ADDI" << std::endl;
+        break;
+    case 0b0011:    //AND
+        std::cout << "AND" << std::endl;
+        break;
+    case 0b0100:    //ANDI
+        std::cout << "ANDI" << std::endl;
+        break;
+    case 0b0101:    //NOT
+        std::cout << "NOT" << std::endl;
+        break;
+    case 0b0110:    //BEQA
+        std::cout << "BEQA" << std::endl;
+        break;
+    case 0b0111:    //BEQR
+        std::cout << "BEQR" << std::endl;
+        break;
+    case 0b1000:    //HLT
+        std::cout << "HLT" << std::endl;
+        break;
+    case 0b1001:    //LB
+        std::cout << "LB" << std::endl;
+        break;
+    case 0b1010:    //SB
+        std::cout << "SB" << std::endl;
+        break;
+    case 0b1011:    //LSR
+        std::cout << "LSR" << std::endl;
+        break;
+    case 0b1100:    //LSL
+        std::cout << "LSL" << std::endl;
+        break;
+    case 0b1101:    //INC
+        std::cout << "INC" << std::endl;
+        break;
+    case 0b1110:    //DEC
+        std::cout << "DEC" << std::endl;
+        break;
+    case 0b1111:    //not used (treated as NOP)
+        std::cout << "extra" << std::endl;
+        break;
     default:
-        std::cout<<"failed"<<std::endl;
+        std::cout << "invalid opcode" << std::endl;
         break;
     }
+    cycles++;
 
 }
 
 uint16_t fetchIns(registers& cpuReg, memory& cpuMem){
     
-    uint16_t ins = cpuMem.ins[cpuReg.PC++]; 
+    uint16_t addr = cpuReg.reg[R_PC] | ((uint16_t)cpuReg.reg[R_PCU] << 8);
+    uint16_t ins = cpuMem.ins[addr];
+    addr++; 
+    cpuReg.reg[R_PC] = (uint8_t)(addr & 0xff);
+    cpuReg.reg[R_PCU] = (uint8_t)((addr>>8) & 0xff);
     return ins;
 
 }
