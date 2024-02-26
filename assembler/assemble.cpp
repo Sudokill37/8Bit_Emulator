e2242f5047b4a36907ce020306852ab40400c9e2
Kevin Fenn <boyce.fenn@gmail.com>
2024-02-25 20:10:59 -0600

    progress on the assembler

--- assembler/assemble.cpp
+++ assembler/assemble.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <bitset>
+
+
+std::vector<std::string> lines;
+std::string line;
+void parseLine(std::string& line, std::ofstream& output_file);
+std::string makeLower(const std::string& input);
+
 
 int main(int argc, char* argv[]) {
     // Check if the correct number of arguments are provided
@@ -14,33 +24,110 @@ int main(int argc, char* argv[]) {
     std::string output_file_name = argv[2];
 
     // Open the input file for reading
-    std::ifstream input_file(input_file_name, std::ios::binary);
+    std::ifstream input_file(input_file_name);
     if (!input_file) {
         std::cerr << "Error: Unable to open input file: " << input_file_name << "\n";
         return 1;
     }
 
-    // Determine the size of the input file in bytes
-    input_file.seekg(0, std::ios::end);
-    std::streampos file_size = input_file.tellg();
-    input_file.seekg(0, std::ios::beg);
-
-    // Open the output file for writing
     std::ofstream output_file(output_file_name);
     if (!output_file) {
         std::cerr << "Error: Unable to open output file: " << output_file_name << "\n";
         return 1;
     }
 
-    // Write the size of the input file to the output file
-    output_file << "Input file size: " << file_size << " bytes\n";
+    while(std::getline(input_file, line)){
+        lines.push_back(line);
+        //std::cout << line << std::endl;
+    }
 
-    std::cout << "Input file size: " << file_size << " bytes\n";
-    std::cout << "File size written to '" << output_file_name << "' successfully.\n";
+    for( auto& line : lines){
+        std::string low = makeLower(line);
+        parseLine(low, output_file);
+    }
+    
+    std::cout <<"test\n";
 
-    // Close files
+
     input_file.close();
     output_file.close();
 
     return 0;
+}
+
+void parseLine(std::string& line, std::ofstream& output_file){
+
+
+
+    std::vector<std::string> tokens;
+    std::istringstream iss(line);
+    std::string token;
+
+    uint8_t opcode = 0;
+    
+    std::vector<std::string> instructions = {"nop", "add", "addi", "and", "andi", "not", "beqa", "beqr", "hlt", "lb", "sb", "lsr", "lsl", "inc", "dec"};
+    
+    while(iss >> token){
+        tokens.push_back(token);
+        
+    }
+
+    if(tokens.size() <= 0){
+        return;
+    }
+
+    for(int i = 0; i < instructions.size(); i++){
+        if(tokens[0]==instructions[i]){
+            opcode = i;
+        }
+    }
+
+    output_file << std::bitset<4>(opcode);
+
+    if(tokens.size() != 4) {
+            std::cerr<<"invalid operands\n";
+    }
+    else{
+           output_file << std::bitset<4>(std::stoi(tokens[1]))<<std::bitset<4>(std::stoi(tokens[2]))<<std::bitset<4>(std::stoi(tokens[3]))<<std::endl;
+    }
+    // switch (opcode){
+    // case 0b0000:
+    //     output_file << std::bitset<12>(0)<<std::endl;
+    //     break;
+    // case 0b0001:
+    //     if(tokens.size() != 4) {
+    //         std::cerr<<"invalid operands\n";
+    //     }
+    //     else{
+    //         output_file << std::bitset<4>(std::stoi(tokens[1]))<<std::bitset<4>(std::stoi(tokens[2]))<<std::bitset<4>(std::stoi(tokens[3]))<<std::endl;
+    //     }
+    //     break;
+    // case 0b0011:
+    //     if(tokens.size() != 4) {
+    //         std::cerr<<"invalid operands\n";
+    //     }
+    //     else{
+    //         output_file << std::bitset<4>(std::stoi(tokens[1]))<<std::bitset<4>(std::stoi(tokens[2]))<<std::bitset<4>(std::stoi(tokens[3]))<<std::endl;
+    //     }
+    //     break;
+    // case 0b1000:
+    //     output_file << std::bitset<16>(0)<<std::endl;
+    //     break;
+    // default:
+    //     std::cerr << "opcode not implemented" << std::endl;
+    // }
+    
+    
+}
+
+ std::string makeLower(const std::string& input) {
+    std::string result;
+    for (char c : input) {
+        if (std::isupper(c)) {
+            result += std::tolower(c);
+        } else {
+            result += c;
+        }
+    }
+    return result;
 }
